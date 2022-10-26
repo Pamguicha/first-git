0e1b9b9b957bb7a626c7996f0955b048350cc65b
Pamguicha <pamelaguicharrousse@gmail.com>
2022-10-27 10:40:22 +1100

    Added API forecast and forecast function

--- src/index.js
+++ src/index.js
@@ -31,7 +31,8 @@ let reloj = document.querySelector(".dayHour");
 let currentTime = new Date();
 reloj.innerHTML = realHour(currentTime);
 
-function displayForecast (){
+function displayForecast(response){
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast-weather");
 
   let forecastHTML = ``;
@@ -66,7 +67,11 @@ days.forEach(function(day){
 }
 
 
-
+function getForecast(coordinates){
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function showTemperature(response) {
     let h1 = document.querySelector(".space");
@@ -88,6 +93,8 @@ function showTemperature(response) {
     let iconElement = document.querySelector("#icon");
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
     iconElement.setAttribute("alt",response.data.weather[0].description );
+
+  getForecast(response.data.coord); 
 
 }
 
@@ -154,4 +161,3 @@ celciusLink.addEventListener("click", showCelciusTemperature);
 
 
 weatherApi("Santiago");
-displayForecast();
